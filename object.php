a4beb15af214e8e7d9dd15561a2fbaceae055b94
Jenn Dykes <jenndykes88@gmail.com>
2017-07-11 21:24:35 -0400

    array example

--- object.php
+++ object.php
@@ -3,40 +3,40 @@
 
 $id = $_REQUEST['id'];
 
-$title = '';
-$event = '';
-$description = '';
-$code = '';
-$date = '';
+$object = array('title' => '', 'event' => '', 'description' => '', 'code' => '', 'date' => '', );
 
 if ($id == 1){ 
-	$title = "Barn Event";
-	$event = "Barn Bash Summer Nights";
-	$description = "Restless Heart";
-	$code = "alert(This is a message)";
-	$date = "August 25, 2017";
-}
+	$object =  array(
+		'title' => 'Barn Event', 
+		'event' => 'Barn Bash Summer Nights',
+		'description' => 'Restless Heart',
+		'code' => 'alert(This is a message)',
+		'date' => 'August 25, 2017',
+		);
+	}
 elseif ($id == 2){
 
-	$title = "Barn Event";
-	$event = "Labor Day Barn Bash";
-	$description = "David Phelps";
-	$code = "alert(This is a message)";
-	$date = "September 4, 2017";
-}
+	$object =  array(
+		'title' => 'Barn Event', 
+		'event' => 'Labor Day Barn Bash',
+		'description' => 'David Phelps',
+		'code' => 'alert(This is a message)',
+		'date' => 'September 4, 2017',
+		);
+	}
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?php echo $title; ?></title>
+		<title><?php echo $object['title']; ?></title>
 	</head>
 	<body>
-		<h1><?php echo $event; ?></h1>
-		<p><?php echo $description; ?></p>
+		<h1><?php echo $object['event']; ?></h1>
+		<p><?php echo $object['description']; ?></p>
 		<pre>
-			<?php echo $code; ?>
+			<?php echo $object['code']; ?>
 		</pre>
-		<p>Date: <?php echo $date; ?></p>
+		<p>Date: <?php echo $object['date']; ?></p>
 	</body>
 </html>
